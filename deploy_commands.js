7b20c7aa17e2faa888101e5096b19a2d3793712c
Denis Karev <dkarev@protonmail.com>
2021-12-14 14:32:47 +0100

    added more choices to deploy_commands.js

--- deploy_commands.js
+++ deploy_commands.js
@@ -10,7 +10,12 @@ const add = new SlashCommandBuilder()
         option.setName('role')
             .setDescription('Название роли')
             .setRequired(true)
-            .addChoice('Rainbow 1', '1'));
+            .addChoice('Rainbow 1', 'Rainbow 1')
+            .addChoice('Rainbow 2', 'Rainbow 2')
+            .addChoice('Rainbow 3', 'Rainbow 3')
+            .addChoice('Rainbow 4', 'Rainbow 4')
+            .addChoice('Rainbow 5', 'Rainbow 5')
+            .addChoice('Rainbow 6', 'Rainbow 6'));
 
 const remove = new SlashCommandBuilder()
     .setName('remove')
